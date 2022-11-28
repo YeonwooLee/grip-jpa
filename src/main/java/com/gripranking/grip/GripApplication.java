69615b54b413ca8732daace7772eb3f883f4cc30
YeonwooLee <fox_93@naver.com>
2022-11-29 01:39:45 +0900

    프로젝트 생성

---
+++ src/main/java/com/gripranking/grip/GripApplication.java
@@ -0,0 +1,13 @@
+package com.gripranking.grip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GripApplication.class, args);
+	}
+
+}
